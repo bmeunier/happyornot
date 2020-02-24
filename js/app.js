951723ffb9765f3f4d82881b6ff285034c5d529b
Ikenna Ojukwu <carteraxwebsolution@gmail.com>
2020-02-24 23:14:53 +0100

    why page implementation

--- js/app.js
+++ js/app.js
@@ -13,16 +13,11 @@ $(document).ready(function() {
 			    measurementId: "G-0NTYN7PQPS"
 			}
 		},
-		storeMood: function(mood, time) {
+		storeMood: function(postData) {
 			// make write request to store logs on firebase
 			var $this = this
 
-			var postData = {
-			    mood: mood,
-			    time: time
-			};
-
-			// Get a key for a new Post.
+			//Get a key for a new Post.
   			var newPostKey = firebase.database().ref().child('moods').push().key;
 
   			var updates = {};
@@ -41,10 +36,6 @@ $(document).ready(function() {
 			// alert message translation
 			return 'There seems to be a problem with your selection';
 		},
-		getMood: function(el) {
-			// html data attr for moods
-			return $(el).data('mood');
-		},
 		getServerTime: function() {
 			// get current date time
 			return moment().format('MMMM-Do-YYYY, h:mm a');
@@ -53,18 +44,44 @@ $(document).ready(function() {
 			// redirect after logging user click
 			window.location = "./tks.html"
 		},
-		init: function() {
-			// START click handler
+		formData: {
+			mood: "", 
+			why: "", 
+			time: ""
+		},
+		getMood: function() {
 			var $this = this;
 
-			firebase.initializeApp(this.firebaseConfig());
-
-			var database = firebase.database();
-
-			$('.faces a').click(function(e) {
+			$(".faces a").click(function(e) {
 				e.preventDefault();
-				$this.storeMood($this.getMood(this), $this.getServerTime());
+				var data = $this.formData;
+
+				// get mood data
+				$this.formData.mood = $(this).data('mood');
+
+				// enter next step
+				$(".section-mood").addClass("animated fadeOutLeft faster");
+				$(".section-why").addClass("animated fadeInRight faster");
+				
 			});
+		},
+		getWhy: function() {
+			var $this = this;
+
+			$(".btn-why__action").click(function(e) {
+				e.preventDefault();
+
+				$this.formData.why = $('.why-value').val();
+				$this.formData.time = $this.getServerTime();
+
+				$this.storeMood($this.formData);
+			});
+		},
+		init: function() {
+			firebase.initializeApp(this.firebaseConfig());
+			var database = firebase.database();
+			this.getMood();
+			this.getWhy();
 		}
 	}
 
